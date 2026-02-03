8dd4939ad1dda93b9f5b2595d14ee953c67e73e3
HenriqueSchulz <141572356+HenriqueSchulz@users.noreply.github.com>
2026-02-03 16:46:55 +0000

    restructuring code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import * as THREE from '../libs/three.module.js';
 import { scene } from './core/scene.js';
 import { camera } from './core/camera.js';
 import { startLoop, onUpdate } from './core/loop.js';
-import { initInput, keys, yaw, pitch } from './core/input.js';
+import { initInput, keys, yaw, pitch } from './player/input.js';
 
 import { buildMap } from './world/map.js';
 import { updatePlayer } from './player/player.js';
